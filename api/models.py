38b8a82b40ab3fe00c12580e584429545cc6673a
meishimomura <mei.shimomura@icloud.com>
2021-03-28 17:50:42 +0900

    modelsの追加とマイグレーション

--- api/models.py
+++ api/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import User
+import uuid
 
-# Create your models here.
+def upload_avatar_path(instance, filename):
+    ext = filename.split('.')[-1]
+    return '/'.join(['avatars', str(instance.user_profile.id) + str(".") + str(ext)])
+
+
+class Profile(models.Model):
+    user_profile = models.OneToOneField(
+        User, related_name='user_profile',
+        on_delete=models.CASCADE
+    )
+    img = models.ImageField(blank=True, null=True, upload_to=upload_avatar_path)
+
+    def __str__(self):
+        return self.user_profile.username
+
+
+class Staff(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    staff_name = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.staff_name
+
+
+class Shift(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    shift_date = models.DateField()
+    shift_start = models.TimeField()
+    shift_end = models.TimeField()
+    staff_name = models.ForeignKey(Staff, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.shift_date,shift_start,shift_end,staff_name
