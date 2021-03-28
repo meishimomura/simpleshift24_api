49c24451f5d6165a5533b73af73c633b58141fb9
meishimomura <mei.shimomura@icloud.com>
2021-03-28 23:45:07 +0900

    views作成

--- api/views.py
+++ api/views.py
@@ -1,3 +1,70 @@
 from django.shortcuts import render
+from rest_framework import status, permissions, generics, viewsets
+from .serializers import UserSerializer, ProfileSerializer, StaffSerializer, ShiftSerializer
+from rest_framework.response import Response
+from .models import Profile, Staff, Shift
+from django.contrib.auth.models import User
+from . import custompermissions
 
-# Create your views here.
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = (permissions.AllowAny,)
+
+
+class ListUserView(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class LoginUserView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
+
+    def update(self, request, *args, **kwargs):
+        response = {'message': 'PUT method is not allowed'}
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ProfileViewSet(viewsets.ModelViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user_profile=self.request.user)
+
+    def destroy(self, request, *args, **kwargs):
+        response = {'message': 'DELETE method is not allowed'}
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
+
+    def partial_update(self, request, *args, **kwargs):
+        response = {'message': 'PATCH method is not allowed'}
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
+
+
+class StaffViewSet(viewsets.ModelViewSet):
+    queryset = Staff.objects.all()
+    serializer_class = StaffSerializer
+    permission_classes = (permissions.IsAuthenticated, custompermissions.OwnerPermission,)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+    def destroy(self, request, *args, **kwargs):
+        response = {'message': 'DELETE method is not allowed'}
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ShiftViewSet(viewsets.ModelViewSet):
+    queryset = Shift.objects.all()
+    serializer_class = ShiftSerializer
+    permission_classes = (permissions.IsAuthenticated, custompermissions.OwnerPermission,)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+    def partial_update(self, request, *args, **kwargs):
+        response = {'message': 'PATCH method is not allowed'}
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
